a0918e3b82d0720d8d177cf31b200545a8ea4b6f
Marcel Medeiros <marcel_segundo@outlook.com>
2023-09-30 10:30:14 +0100

    Step 5 completed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("java")
+    id("application")
 }
 
-group = "com.marcelmedeiros1.lab2"
+application {
+    mainClass.set("Application")
+}
+group = "org.marcelmedeiros1.hero"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +16,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    //https://mvnrepository.com/artifact/com.googlecode.lanterna/lanterna
+    implementation("com.googlecode.lanterna", "lanterna", "3.1.1")
 }
 
 tasks.test {
